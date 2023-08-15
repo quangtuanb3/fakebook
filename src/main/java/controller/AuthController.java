bf2c2648a56153bd03737d3b5d163705c0f34ea3
ANHTUANPHUNG <132957644+ANHTUANPHUNG@users.noreply.github.com>
2023-08-15 08:11:50 +0700

    TuanEm git 15/8

--- src/main/java/controller/AuthController.java
+++ src/main/java/controller/AuthController.java
@@ -6,14 +6,21 @@ import Model.Profile;
 import Model.User;
 import Utils.AppUtil;
 import services.AuthService;
+import services.ProfileService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.Data;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.UUID;
+
+import static Utils.AppUtil.getObjectWithValidation;
 
 @WebServlet(urlPatterns = "/auths", name = "authController")
 public class AuthController extends HttpServlet {
@@ -50,18 +57,41 @@ public class AuthController extends HttpServlet {
     }
 
     private void register(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = (User) AppUtil.getObjectWithValidation(req, User.class, new HashMap<>());
-        Profile profile = (Profile) AppUtil.getObjectWithValidation(req, Profile.class, new HashMap<>());
-        assert user != null;
-        assert profile != null;
+        User user = getUserWithValidationUser(req);
+        Profile profile = getObjectWithValidationPass(req);
         authService.register(user, profile);
         req.setAttribute("message", "Register successfully");
         req.getRequestDispatcher("auths/login.jsp").forward(req, resp);
     }
 
+    private Profile getObjectWithValidationPass(HttpServletRequest req) {
+        String name = req.getParameter("name");
+        String phone = req.getParameter("phone");
+        String avatar = req.getParameter("avatar");
+        Date dob = Date.valueOf(req.getParameter("dob"));
+        EGender gender = EGender.valueOf(req.getParameter("gender"));
+        String cover = req.getParameter("cover");
+      Profile profile = new Profile();
+      profile.setName(name);
+      profile.setPhone(phone);
+      profile.setName(avatar);
+      profile.setDob(dob);
+      profile.setGender(gender);
+      profile.setCover(cover);
+      return profile;
+    }
+
+    private User getUserWithValidationUser(HttpServletRequest req) {
+//        Integer id =Integer.valueOf(req.getParameter("id"));
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        User user = new User(email, password);
+        return user;
+    }
+
     private void login(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         try {
-            User user = (User) AppUtil.getObjectWithValidation(req, User.class, new HashMap<>());
+            User user = (User) getObjectWithValidation(req, User.class, new HashMap<>());
             assert user != null;
             String destinationPage = authService.login(user, req);
             resp.sendRedirect(destinationPage);
